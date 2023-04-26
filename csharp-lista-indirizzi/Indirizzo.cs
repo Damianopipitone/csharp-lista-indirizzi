903a5ce545b99769b248323377d355dba081b60f
Damianopipitone <damianopipitone33@gmail.com>
2023-04-26 15:56:49 +0200

    Completata la classe indirizzo

--- csharp-lista-indirizzi/Indirizzo.cs
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,60 @@ namespace csharp_lista_indirizzi
         public string province;
         public int ZIP;
         List<Indirizzo> lista;
-    }
+        
+        // COSTRUTTORE
 
-    // COSTRUTTORE
-}
+        public Indirizzo(string name, string surname, string street, string city, string province, int ZIP)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.ZIP = ZIP;
+            this.lista = new List<Indirizzo>();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetSurname()
+        {
+            return surname;
+        }
+
+        public string GetStreet()
+        {
+            return street;
+        }
+
+        public string GetCity()
+        {
+            return city;
+        }
+
+        public int GetZIP()
+        {
+            return ZIP;
+        }
+
+        // METODI 
+
+        public override string ToString()
+        {
+            string addressInfo = "-----------------------------";
+            addressInfo += "\tNome: " + this.name + "\n";
+            addressInfo += "\tCognome: " + this.surname + "\n";
+            addressInfo += "\tStrada: " + this.street + "\n";
+            addressInfo += "\tCittà: " + this.city + "\n";
+            addressInfo += "\tNumero civico: " +  this.ZIP + "\n";
+            return addressInfo;
+        }
+    }   
+
+
+    
+}   
+    
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,8 +4,35 @@ using csharp_lista_indirizzi;
 using static System.Net.Mime.MediaTypeNames;
 using System;
 
-StreamReader file = File.OpenText("C:\\Users\\Utente\\Desktop\\esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\file di testo\\addresses.csv");
-
-Indirizzo address = new Indirizzo();
-
 List<Indirizzo> addressList = new List<Indirizzo>();
+
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\Utente\\Desktop\\esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\file di testo\\addresses.csv");
+
+    int rowCounter = 0;
+
+    while (!file.EndOfStream)
+    {
+        string rowRead = file.ReadLine();
+        rowCounter++;
+
+        if (rowCounter > 1)
+        {
+            string[] splitInfo = rowRead.Split(",");
+
+            if (splitInfo.Length != 6)
+            {
+                Console.WriteLine("La riga " + rowCounter + "non rispetta i parametri previsit!");
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    
+
+        
+}
