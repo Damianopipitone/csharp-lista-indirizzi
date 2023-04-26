61c0f1151b52d5e9136157536ce4321971918a73
Damianopipitone <damianopipitone33@gmail.com>
2023-04-26 15:08:32 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string name;
+        public string surname;
+        public string street;
+        public string city;
+        public string province;
+        public int ZIP;
+        List<Indirizzo> lista;
+    }
+
+    // COSTRUTTORE
+}
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using csharp_lista_indirizzi;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+StreamReader file = File.OpenText("C:\\Users\\Utente\\Desktop\\esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\file di testo\\addresses.csv");
+
+Indirizzo address = new Indirizzo();
+
+List<Indirizzo> addressList = new List<Indirizzo>();
